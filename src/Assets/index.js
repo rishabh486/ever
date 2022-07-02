bfecccbf436b883cbdc7e91daf5666f7f9802180
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2022-07-02 23:47:27 +0530

    changes-in-box

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -2,4 +2,5 @@ import image from "./Images/image.png";
 import avatar from "./Images/avatar.png";
 import design from "./Images/design.png";
 import revenue from "./Images/revenue.png";
-export { image, avatar, design, revenue };
+import arrow from "./Images/arrow.png";
+export { image, avatar, design, revenue, arrow };
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -6,23 +6,45 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Avatar } from "@mui/material";
-import { revenue } from "../Assets";
+import { revenue, arrow } from "../Assets";
 
 export default function BCard() {
   return (
-    <Card style={{}} sx={{ width: 190, height: 170, borderRadius: 6 }}>
+    <Card
+      style={{ backgroundColor: "rgba(255, 255, 255, 1)" }}
+      sx={{ width: 200, height: 170, borderRadius: 6 }}
+    >
       <CardContent>
         <Avatar src={revenue} />
         <Typography
-          sx={{ fontSize: 18, fontFamily: "poppins" }}
+          sx={{ fontSize: 17, fontFamily: "poppins" }}
           color="rgba(112, 112, 112, 1)"
           gutterBottom
         >
           Gross Revenue
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="rgba(112, 112, 112, 1)">
+        <Typography
+          sx={{ fontSize: 15, fontFamily: "poppins" }}
+          color="rgba(0, 0, 0, 1)"
+        >
           $48,670.19
         </Typography>
+        <div className="box-layer">
+          <img style={{ width: 10, height: 10 }} src={arrow} />
+          <Typography
+            sx={{ fontSize: 15, fontFamily: "poppins" }}
+            color="
+            rgba(14, 176, 0, 1)"
+          >
+            10.30%
+          </Typography>
+          <Typography
+            sx={{ fontSize: 15, fontFamily: "poppins" }}
+            color="rgba(0, 0, 0, 1)"
+          >
+            from last week
+          </Typography>
+        </div>
       </CardContent>
     </Card>
   );
